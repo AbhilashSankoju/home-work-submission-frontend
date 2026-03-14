50613f7cbe1d38ba1217578a9223d4048de17370
AbhilashSankoju <sankojuabhilash5@gmail.com>
2026-03-14 19:08:25 +0530

    Delete eduportal-v3 directory

--- eduportal-v3/src/services/assignmentService.js
+++
@@ -1,8 +0,0 @@
-import api from './api'
-
-export const assignmentService = {
-  getAll:  ()       => api.get('/assignments').then(r => r.data),
-  getById: (id)     => api.get(`/assignments/${id}`).then(r => r.data),
-  create:  (data)   => api.post('/assignments', data).then(r => r.data),
-  getMine: ()       => api.get('/assignments/my').then(r => r.data),
-}
